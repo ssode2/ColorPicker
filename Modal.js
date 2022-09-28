d4481dff3d3077e42525068d18c444041f964f4a
ssode2 <108406271+ssode2@users.noreply.github.com>
2022-09-28 19:03:33 +0300

    JsFile

---
+++ Modal.js
@@ -0,0 +1,38 @@
+function get_rgb() {
+  var R = document.getElementById('RGB_R').value;
+  var G = document.getElementById('RGB_G').value;
+  var B = document.getElementById('RGB_B').value;
+
+}
+function get_hls() {
+  var H = document.getElementById('HLS_H').value;
+  var L = document.getElementById('HLS_L').value;
+  var S = document.getElementById('HLS_S').value;
+
+}
+function get_hsv() {
+  var H = document.getElementById('HSV_H').value;
+  var S = document.getElementById('HSV_S').value;
+  var V = document.getElementById('HSV_V').value;
+
+}
+function get_xyz() {
+  var X = document.getElementById('XYZ_X').value;
+  var Y = document.getElementById('XYZ_Y').value;
+  var Z = document.getElementById('XYZ_Z').value;
+
+}
+function get_lab() {
+  var L = document.getElementById('LAB_L').value;
+  var A = document.getElementById('LAB_A').value;
+  var B = document.getElementById('LAB_B').value;
+
+}
+function get_cmyk() {
+  var C = document.getElementById('CMYK_C').value;
+  var M = document.getElementById('CMYK_M').value;
+  var Y = document.getElementById('CMYK_Y').value;
+  var K = document.getElementById('CMYK_K').value;
+
+}
+
